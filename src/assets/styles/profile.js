0fe79559512740f03da6ec352c7c04827e291627
Jefferson <jefferson_ribeiro12345@hotmail.com>
2019-10-16 18:23:03 -0300

    adding styling and more components for page.

--- src/assets/styles/profile.js
+++ src/assets/styles/profile.js
@@ -1,54 +1,104 @@
-import { StyleSheet } from  'react-native'
+import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-    inforProfile: {
-        position:"relative",
-        bottom:230,
-        margin:10,
-        flexDirection:"row",
-    },
-    inforProfileTtext: {
-        flex:2,
-        fontSize:11,
-        margin:8,
-        fontWeight:'bold',
-        justifyContent:"center",
-        alignItems:"center",
-        alignSelf:"center",
-    },
-    inforProfileNumber: {
-        fontSize:19,
-        fontWeight:'bold',
-    },
-    inforProfileImage: {
-        marginRight:3,
-        marginLeft:5,
-        marginTop:59,
-        height:110, 
-        width:110, 
-        borderRadius:110,
-        backgroundColor:'darkgray'
-    },
-    inforProfileButtons: {
-        borderColor:'darkgray',
-        borderStyle:'solid',
-        borderWidth:1,
-        borderRadius: 5,
-        marginHorizontal:1,
-        padding:6,
+  pageProfile: {
+    flex: 10,
+    justifyContent: 'flex-start',
+  },
+  inforProfile: {
+    flexDirection: 'row',
+    height: '15%',
+  },
+  inforProfileTtext: {
+    flex: 1,
+    fontSize: 11,
+    display: 'flex',
+    margin: 8,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    textAlign: 'center',
+  },
+  inforProfileNumber: {
+    fontSize: 19,
+    fontWeight: 'bold',
+  },
+  inforProfileImage: {
+    margin: 7,
+    height: 110,
+    width: 110,
+    borderRadius: 110,
+    backgroundColor: 'darkgray',
+  },
+  inforProfileButtons: {
+    borderColor: 'darkgray',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginHorizontal: 5,
+  },
+  inforProfileButtonsText: {
+    fontSize: 15,
+    marginHorizontal: 13,
+    fontWeight: 'bold',
+  },
+  inforProfileButtonslayout: {
+    width: '95%',
+    justifyContent: 'flex-end',
+    flexDirection: 'row',
+    marginHorizontal: 5,
+  },
+  iconStyle: {
+    position: 'relative',
+    right: '150%',
+    top: '95%',
+    backgroundColor: 'skyblue',
+    color: 'white',
+    alignSelf: 'flex-end',
+    textAlign: 'center',
+    height: 30,
+    width: 30,
+    borderRadius: 30,
+  },
+  ProfileDescriptions: {
+    flex: 1.8,
+    margin: 16,
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  ProfileDescriptionsText: {
+    fontSize: 16,
+  },
+  ProfileFeatured: {
+    flex: 1.8,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+  profileFeaturedImage: {
+    backgroundColor: 'darkgray',
+    height: '90%',
+    width: '21%',
+    borderRadius: 50,
+  },
+  ProfileContacts: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderColor: 'lightgray',
+  },
+  ProfileContactsText: {
+    color: 'skyblue',
+    marginHorizontal: 20,
+    fontSize: 18,
+  },
+  profilePosts: {
+    flex: 4,
+  },
+});
 
-    }, 
-    inforProfileButtonsText :{
-        fontSize:15,
-        marginHorizontal:13,
-        fontWeight:'bold'
-    },
-    inforProfileButtonslayuot: {
-        flexDirection:'row',
-        position:'relative',
-        bottom:290,
-        left:60,
-    }
-})
-
-export default styles
+export default styles;
--- src/screens/profile.js
+++ src/screens/profile.js
@@ -1,38 +1,80 @@
 import React, {Component} from 'react';
-import {View, Text, Image, TouchableOpacity} from'react-native'
-import styles from '../assets/styles/profile'
+import {View, Text, Image, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-ionicons';
+import styles from '../assets/styles/profile';
+import {createDrawerNavigator} from 'react-navigation-drawer';
+import {createAppContainer} from 'react-navigation';
 
-export default class Profile extends Component {
-    render() {
-      return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-
-          <View style={styles.inforProfile}>
-          <Image
-            style={styles.inforProfileImage}
-           />
-            <Text style={styles.inforProfileTtext}>
-              <Text style={styles.inforProfileNumber}>  349    </Text>
-              publicações
-            </Text>
-            <Text style={styles.inforProfileTtext}>
-            <Text style={styles.inforProfileNumber}>145 mil</Text>
-                seguidores
-            </Text>
-            <Text style={styles.inforProfileTtext}>
-              <Text style={styles.inforProfileNumber}>1.469  </Text>
-              seguindo
-            </Text>
-          </View>
-          <View style={styles.inforProfileButtonslayuot}>
-            <TouchableOpacity style={styles.inforProfileButtons}>
-              <Text style={styles.inforProfileButtonsText}>Promoçoes</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.inforProfileButtons}>
-              <Text style={styles.inforProfileButtonsText}>Editar perfil</Text>
-            </TouchableOpacity>
-          </View>
+class Profile extends Component {
+  render() {
+    return (
+      <View style={styles.pageProfile}>
+        <View style={styles.inforProfile}>
+          <Image style={styles.inforProfileImage} />
+          <TouchableOpacity>
+            <Icon style={styles.iconStyle} ios="ios-add" android="md-add" />
+          </TouchableOpacity>
+          <Text style={styles.inforProfileTtext}>
+            <Text style={styles.inforProfileNumber}> 349 </Text>
+            publicações
+          </Text>
+          <Text style={styles.inforProfileTtext}>
+            <Text style={styles.inforProfileNumber}>14 mil </Text>
+            seguidores
+          </Text>
+          <Text style={styles.inforProfileTtext}>
+            <Text style={styles.inforProfileNumber}>1.469 </Text>
+            seguindo
+          </Text>
         </View>
-      );
-    }
+        <View style={styles.inforProfileButtonslayout}>
+          <TouchableOpacity style={styles.inforProfileButtons}>
+            <Text style={styles.inforProfileButtonsText}>Promoçoes</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.inforProfileButtons}>
+            <Text style={styles.inforProfileButtonsText}>Editar perfil</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.ProfileDescriptions}>
+          <Text style={styles.ProfileDescriptionsText,{fontWeight:'bold'}}>Bume</Text>
+          <Text style={styles.ProfileDescriptionsText, {color:'darkgray'}}>Serviço de automação</Text>
+          <Text style={styles.ProfileDescriptionsText,{color:'skyblue'}}>#Bume #MidiasSociais</Text>
+          <Text style={styles.ProfileDescriptionsText,{color:'skyblue'}}>Belo Horizonte, Brazil</Text>
+        </View>
+        <View style={styles.ProfileFeatured}>
+        <Image style={styles.profileFeaturedImage} /> 
+        <Image style={styles.profileFeaturedImage} />
+        <Image style={styles.profileFeaturedImage} />
+        <Image style={styles.profileFeaturedImage} />
+        </View>
+        <View style={styles.ProfileContacts}>
+          <TouchableOpacity>
+            <Text style={styles.ProfileContactsText}>Enviar Email</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.ProfileContactsText}>Como Chegar</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.profilePosts}></View>
+      </View>
+    );
   }
+}
+
+const MyDrawerNavigator = createDrawerNavigator(
+  {
+    Profile: {
+      screen: Profile,
+    },
+  },
+  {
+    drawerPosition: 'right',
+    drawerWidth: 250,
+    statusBarAnimation: 'fade',
+    drawerType: 'slide',
+  },
+);
+
+const MyApp = createAppContainer(MyDrawerNavigator);
+
+export default MyApp;
