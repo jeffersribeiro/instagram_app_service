e2c98493355ac91a8cb2c1927d68d85a10727338
Jefferson <jefferson_ribeiro12345@hotmail.com>
2019-10-30 17:57:01 -0300

    "Home" page modification to test the scroll view component.

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,13 +1,26 @@
 import React, {Component} from 'react';
-import {View, Text} from'react-native'
-
+import {View, Text, ScrollView, SafeAreaView, StyleSheet} from 'react-native';
+import Stories from '../components/stories/index'
+import styles from '../assets/styles/home'
 
 export default class Home extends Component {
-    render() {
-      return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Text>Home!!!</Text>
-        </View>
-      );
-    }
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <ScrollView style={styles.scrollView}>
+          <Stories />
+          <Text style={styles.text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Text>
+        </ScrollView>
+      </SafeAreaView>
+    );
   }
+}
+
